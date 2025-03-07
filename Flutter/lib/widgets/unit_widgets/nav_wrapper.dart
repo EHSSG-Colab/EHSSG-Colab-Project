c4d0e17f3797c98ff7d85fa42cac6853e9e1cd3b
Ye Thi Ha Htwe <yethihahtwe319@gmail.com>
2025-03-07 04:45:38 +0000

    Fri, Mar 7, 2025, 11:45 AM +07:00

--- Flutter/lib/widgets/unit_widgets/nav_wrapper.dart
+++ Flutter/lib/widgets/unit_widgets/nav_wrapper.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class NavWrapper extends StatefulWidget {
-  const NavWrapper({super.key});
+  // Navigation wrapper is like an array. It starts with 0. The first item of the navigation menu will have an index of 0, the second and index of 1, etc.
+  // By default, the index page as well as the landing page of the navigation wrapper is going to be the first page. Its index would be 0.
+  final int initialIndex;
+  const NavWrapper({super.key, this.initialIndex = 0});
 
   @override
   State<NavWrapper> createState() => _NavWrapperState();

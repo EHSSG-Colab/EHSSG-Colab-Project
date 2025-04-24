ccab622fe98ae3b59c9b786da87c83fa6e273505
Ye Thi Ha Htwe <108426020+yethihahtwe@users.noreply.github.com>
2025-04-24 16:21:08 +0700

    Reverted changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,12 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        \URL::forceRootUrl(config('app.url'));
-
-        // If using a subdirectory
-        \URL::forceScheme('https');
-
-        // You might also need this to handle the subdirectory
-        $this->app['request']->server->set('SCRIPT_NAME', '/internship/25-01/index.php');
+       //
     }
 }

3f8e7b6750b97103126c8eeeffde531717e5bf70
Ye Thi Ha Htwe <yethihahtwe319@gmail.com>
2025-05-13 09:16:33 +0700

    Changed maximum page width to Full

--- app/Filament/Resources/MalariaCaseReportResource/Pages/ListMalariaCaseReports.php
+++ app/Filament/Resources/MalariaCaseReportResource/Pages/ListMalariaCaseReports.php
@@ -5,10 +5,16 @@ namespace App\Filament\Resources\MalariaCaseReportResource\Pages;
 use App\Filament\Resources\MalariaCaseReportResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Support\Enums\MaxWidth;
 
 class ListMalariaCaseReports extends ListRecords
 {
     protected static string $resource = MalariaCaseReportResource::class;
+
+    public function getMaxContentWidth(): MaxWidth
+    {
+        return MaxWidth::Full;
+    }
 
     protected function getHeaderActions(): array
     {

b6cd04f45595752df9443432c5a3a9280ea7f494
Ye Thi Ha Htwe <yethihahtwe319@gmail.com>
2025-05-13 09:45:19 +0700

    Inserted values to StatsOverview

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\MalariaCaseReport;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -9,8 +10,35 @@ class StatsOverview extends BaseWidget
 {
     protected function getStats(): array
     {
+        // TOTAL CASES
+        $totalCases = MalariaCaseReport::query()
+            ->count();
+
+        // TOTAL POSITIVE CASES
+        $totalPositiveCases = MalariaCaseReport::query()
+            ->where('rdt_result', true)->count();
+
+        // POSITIVE RATE
+        $positiveRate = $totalCases > 0 ?
+            number_format(($totalPositiveCases / $totalCases) * 100, 2) . "%"
+            : "0.00%";
+
         return [
-            //
+            // TOTAL CASES
+            Stat::make(
+                label: 'Total cases',
+                value: $totalCases
+            ),
+            // TOTAL POSITIVE CASES
+            Stat::make(
+                label: 'Totao positive cases',
+                value: $totalPositiveCases,
+            ),
+            // POSITIVE RATE
+            Stat::make(
+                label: 'Positive rate',
+                value: $positiveRate,
+            )
         ];
     }
 }
